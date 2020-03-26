df1e2e7b14c7621c5e27ef639f2116d0f0f47e22
rohit <rohitsdec4@gmail.com>
2020-03-26 19:29:45 +0530

    Modified merge function, changed default name and path for merged pdf

--- mergePDF.py
+++ mergePDF.py
@@ -7,9 +7,6 @@ def merge(pathToFolder=None, filename=None):
     if pathToFolder is None:
         print("Error: No Path given")
         return False
-    if Path(filename).suffix != ".pdf":
-        print("Error: Invalid File Name")
-        return False
     files = sorted(listdir(pathToFolder))
     if len(files) < 2:
         print("Error: Not enough files to merge")
@@ -22,7 +19,10 @@ def merge(pathToFolder=None, filename=None):
             mergedObject.append(PdfFileReader(join(pathToFolder, name)))
     if filename is None:
         d = dt.now()
-        filename = join(pathToFolder, "nbt_{}_{}_{}.pdf".format(d.date, d.month, d.year))
+        filename = d.strftime("%d %B %Y.pdf")
+    if Path(filename).suffix != ".pdf":
+        print("Error: Invalid File Name")
+        return False
     print("Status: Creating merged file '%s'" % filename)
     mergedObject.write(filename)
     print("Status: Finished")
