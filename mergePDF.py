49b71e2f02f07d89a0a00b1427a3d8202eba5048
Rohit <rohitsdec4@gmail.com>
2024-09-24 19:46:31 +0530

    add compress_pdf method

--- mergePDF.py
+++ mergePDF.py
@@ -1,9 +1,9 @@
 from datetime import datetime as dt
-from os import listdir
-from os.path import join
+from os import listdir, stat
+from os.path import join, exists
 from pathlib import Path
 
-from PyPDF2 import PdfMerger, PdfReader
+from PyPDF2 import PdfMerger, PdfReader, PdfWriter
 
 
 def merge(pathToFolder,filename):
@@ -42,3 +42,34 @@ def merge(pathToFolder,filename):
         return False
 
     return True
+
+def get_file_size(path):
+    size = -1
+    if exists(path):
+        st_size = stat(path).st_size
+        size = st_size // (1024 * 1024)
+
+    return size
+
+def compress_pdf(file_to_compress, final_filename):
+    try:
+        if not file_to_compress:
+            raise Exception(f"path-{file_to_compress} missing for the file to be compressed")
+
+        print(f"compress_pdf: starting compression for {file_to_compress}. Initial size: {get_file_size(file_to_compress)}")
+        reader = PdfReader(file_to_compress)
+        writer = PdfWriter()
+        count = 1
+        for page in reader.pages:
+            page.compress_content_streams()
+            writer.add_page(page)
+            print(f"compress_pdf: compressed page {count}")
+            count += 1
+
+        with open(final_filename, "wb") as f:
+            writer.write(f)
+
+        print("compress_pdf: successfully compressed")
+    except Exception as compress_pdf_err:
+        print(f"Error (compress_pdf): {compress_pdf_err}")
+
